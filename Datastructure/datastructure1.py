6d1768e14dcad98645aff878e7b4579ca1ec2066
sunilsala88 <sunilsala88@gmail.com>
2024-08-03 16:58:31 +0530

    nnn

---
+++ Datastructure/datastructure1.py
@@ -0,0 +1,17 @@
+
+
+#list
+
+a=[1,2,3,'amzn','goog','tsla']
+print(a)
+
+#indexing
+print(a[3])
+
+#slicing
+print(a[3:-1])
+
+#slicing formula
+# var_name[start:end+1]
+# 5=end_ind+1
+# end_ind=5-1=4
