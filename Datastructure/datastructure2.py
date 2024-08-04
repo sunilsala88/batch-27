905b67904ee51881366003a314b32fb508780553
sunilsala88 <sunilsala88@gmail.com>
2024-08-04 17:04:39 +0530

    new

---
+++ Datastructure/datastructure2.py
@@ -0,0 +1,17 @@
+
+#dictionary
+#mutable
+stock_prices={"amzn":100,'tsla':200,'goog':600}
+
+print(stock_prices['tsla'])
+
+
+print(stock_prices.get('nifty'))
+
+# a='hello'
+# print(a[9])
+
+
+a=[1,2,3]
+print(a.append(5))
+print(a)
