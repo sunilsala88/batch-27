97901250e846ccfcce7b4d5d596051d465b936d6
sunilsala88 <sunilsala88@gmail.com>
2024-08-17 17:20:57 +0530

    new

--- loops/loops1.py
+++ loops/loops1.py
@@ -1,24 +1,42 @@
 
-
-list1=[4,5,1,2]
-
-#type 1
-for item in list1:
-    if item%2!=0:
-        print(item, ' is odd')
+list1=['goog','amzn','tsla']
 
 
-# string1='hello'
-
-# for char in string1:
-#     print(char)
-
-total=0
-for a in list1:
-    total=total+a
-print(total)
+# #type 1
+#to go through each elem of list
+for i in list1:
+    print(i)
 
 #type 2
-# print(list(range(10)))
-# print(list(range(10,20)))
-# print(list(range(10,50,5)))
+#to repeat a code for n number of time
+for i in range(100):
+    print('hello')
+
+
+#print all number from 1 to 100
+number=0
+for i in range(100):
+    number=number+1
+    print(number)
+    
+for  i in range(100):
+    print(i)
+
+#type 3
+#to go through each elem of list by using index (range function)
+
+for i in range(len(list1)):
+    print(list1[i])
+    # print(i)
+
+
+name=['tsla','goog','nifty']
+prices=[55,77,88]
+
+stock_prices={}
+
+for i in range(len(name)):
+    stock_prices.update({name[i]:prices[i]})
+    print(name[i],prices[i])
+
+print(stock_prices)
