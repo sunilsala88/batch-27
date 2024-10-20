be61a5bacbfc2830e7cb88d790510a551dccbc8a
sunilsala88 <sunilsala88@gmail.com>
2024-10-20 18:07:04 +0530

    oct-20-2

---
+++ financial data/fundamental.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+
+ticker_obj=yf.Ticker('RELIANCE.NS')
+# d=ticker_obj.info
+# print(d.keys())
+# for i in d.keys():
+#     print(i)
+
+# print(d.get('beta'))
+
+df=ticker_obj.get_income_stmt()
+print(df)
